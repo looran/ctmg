8bcc7bfb990a1e597e090cc567abd9d4a7c67c1d
Jason A. Donenfeld <Jason@zx2c4.com>
2015-10-22 18:32:12 +0200

    Quotation is important in bash

--- ctmg.sh
+++ ctmg.sh
@@ -42,7 +42,7 @@ fatal() {
 }
 trace() {
 	echo "[-] $@"
-	eval $@
+	eval "$@"
 }
 yesno() {
 	[[ -t 0 ]] || return 0
@@ -73,54 +73,54 @@ do_new() {
 	trace sudo cryptsetup --cipher aes-xts-plain64 \
 		--key-size 512 --hash sha512 --iter-time 5000 \
 		--use-random --verify-passphrase \
-		luksFormat $container_path
+		luksFormat "$container_path"
 	do_open 0
-	trace sudo mkfs.ext4 $mapper_path
+	trace sudo mkfs.ext4 "$mapper_path"
 	do_open 1
 }
 
 do_delete() {
-	trace rm -f $container_path
+	trace rm -f "$container_path"
 }
 
 do_open() {
 	do_mount=$1
-	if [ -e $mapper_path ]; then
+	if [[ -e $mapper_path ]]; then
 		echo "Mapper file $mapper_path already exists, not reopening"
 	else
-		trace losetup_path=$(sudo losetup --show -f $container_path)
-		trace sudo cryptsetup luksOpen $losetup_path $mapper_name
+		trace losetup_path=$(sudo losetup --show -f "$container_path")
+		trace sudo cryptsetup luksOpen "$losetup_path" "$mapper_name"
 	fi
-	if [ $do_mount -eq 1 ]; then
-		trace mkdir -p $mount_path
-		trace sudo mount /dev/mapper/$mapper_name $mount_path
-		trace sudo chown "$(id -u):$(id -g)" $mount_path # XXX optional
+	if [[ $do_mount -eq 1 ]]; then
+		trace mkdir -p "$mount_path"
+		trace sudo mount "/dev/mapper/${mapper_name}" "$mount_path"
+		trace sudo chown "$(id -u):$(id -g)" "$mount_path" # XXX optional
 	fi
 }
 
 do_close() {
-	trace sudo umount $mount_path && true
-	trace rmdir $mount_path && true
+	trace sudo umount "$mount_path" && true
+	trace rmdir "$mount_path" && true
 	trace sudo cryptsetup luksClose $mapper_name
 	trace losetup_path=$(sudo losetup -l | grep "$container_path" | awk '{print $1}')
-	trace sudo losetup -d $losetup_path
+	trace sudo losetup -d "$losetup_path"
 }
 
 do_list() {
-	lsblk -l |grep $CT_MAPPER_PREFIX
+	lsblk -l | grep "$CT_MAPPER_PREFIX"
 }
 
 #
 # MAIN
 #
 
-PROGRAM="$(basename $0)"
+PROGRAM="$(basename "$0")"
 set -e
 
-case $1 in
+case "$1" in
 n|new)
 	[[ $# -ne 3 ]] && fatal_usage
-	shift && readarg_container $1
+	shift && readarg_container "$1"
 	container_size="$2"
 	do_new
 	echo "[*] Created $container_path of size ${container_size}MB"
@@ -128,7 +128,7 @@ n|new)
 	break;;
 d|del|delete)
 	[[ $# -ne 2 ]] && fatal_usage
-	shift && readarg_container $1
+	shift && readarg_container "$1"
 	yesno "Are you sure you would like to delete $container_path?"
 	do_close && true
 	do_delete
@@ -136,7 +136,7 @@ d|del|delete)
 	break;;
 c|close)
 	[[ $# -ne 2 ]] && fatal_usage
-	shift && readarg_container $1
+	shift && readarg_container "$1"
 	do_close
 	echo "[*] Closed and unmounted $mount_path"
 	break;;
@@ -150,7 +150,7 @@ help|-h)
 *)
 	[[ $# -lt 1 ]] && fatal_usage
 	[[ $1 == "open" || $1 == "o" ]] && shift
-	readarg_container $1
+	readarg_container "$1"
 	do_open 1
 	echo "[*] Opened and mounted $mount_path"
 	break;;
